5e14c93b3d98d025cf8580fce83e87a735cd090a
Kevin Gomes <kb.developer.br@gmail.com>
2023-06-09 11:17:36 -0300

    init repository ws-recaptcha-enterprise-rest

---
+++ ws-recaptcha-enterprise-rest/src/main/java/br/com/kbmg28/wsrecaptchaenterpriserest/WsRecaptchaEnterpriseRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.kbmg28.wsrecaptchaenterpriserest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRecaptchaEnterpriseRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRecaptchaEnterpriseRestApplication.class, args);
+	}
+
+}
---
+++ ws-recaptcha-enterprise-rest/src/test/java/br/com/kbmg28/wsrecaptchaenterpriserest/WsRecaptchaEnterpriseRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kbmg28.wsrecaptchaenterpriserest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRecaptchaEnterpriseRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
